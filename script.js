ff6ba60bcb14275e5eb3eebb84d564f5f958b6fa
Luiz Fernando <96695552+Luizfernando022@users.noreply.github.com>
2022-05-23 15:57:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,20 @@
 let input = document.getElementById('input')
 let buttonCriptografar = document.getElementById('cript')
 let result = document.getElementById('result')
+let resultText = document.getElementById('resultText')
+let copyButton = document.getElementById('copy')
+let array;
+
+function texte(){
+
+    if(resultText.children[0].value == ''){
+        return
+    }
+    let content = resultText.children[0]
+    content.select()
+    document.execCommand('copy')
+    
+}
 
 
 buttonCriptografar.addEventListener('click',showValue)
@@ -11,20 +25,51 @@ function showValue(){
 
     let value = input.value
 
-    let resultText = document.getElementById('resultText')
+    array = value.split('')
+
+
+    
     let copyBtn = document.getElementById('copy')
     copyBtn.style.display = "flex"
-    resultText.innerHTML = `<p>${value}</p>`
+    resultText.innerHTML = `<p>${cripto(array)}</p>`
 
 
 
 }
 
 
-let luiz = [0,1,2,3]
+function cripto(array){
+ let num = 0
+ array.forEach(element=>{
+     verify(element,num)
+     num++
+ })
+ let text = array.join('')
 
-function separar(e){
-    
+ return text
+
 }
 
-console.log(luiz.map(separar))
+function verify(e,num){
+    e = e
+    if(e == "a"){
+        e = "a01100001"
+        array[num] = e
+    }
+    else if(e == "e"){
+        e = "e01100101"
+        array[num] = e
+    }
+    else if(e == "i"){
+        e = "i01101001"
+        array[num] = e
+    }
+    else if(e == "o"){
+        e = "o01101111"
+        array[num] = e
+    }
+    else if(e == "u"){
+        e = "u01110101"
+        array[num] = e
+    }
+}
