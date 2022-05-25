afaef755e0f9d5707956f7f8520fe05ef46aea08
Luiz Fernando <96695552+Luizfernando022@users.noreply.github.com>
2022-05-25 12:06:35 -0500

    attjs

--- script.js
+++ script.js
@@ -23,8 +23,12 @@ function showValue(){
 
 let luiz = [0,1,2,3]
 
-function separar(e){
-    
-}
+let texto1 = "Luiz Fernando"
+let texto = texto1.length
+let array = []
 
-console.log(luiz.map(separar))
+// for(let i = 0;i < texto;i++){
+//     array.push(texto.slice(i))
+// }
+
+console.log(texto1.slice(1))
